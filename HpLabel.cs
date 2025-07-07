3464d1b26d0a417a90f4f12ff26e146bcd2e1282
Cheesypockets <ken020789@gmail.com>
2025-07-06 20:22:43 -0400

    Adding Readme Resource Catagory
    This will eventually be for readmes that each branch will have for their own pieces of information related to the branch project.

--- HpLabel.cs
+++ HpLabel.cs
@@ -3,9 +3,5 @@ using System;
 
 public partial class HpLabel : Label
 {
-    /// <summary>
-    /// Fires an event when HP is changed to a character
-    /// </summary>
-    [Signal]
-    public delegate void onHPChangeEventHandler();
+
 }
