0b491ae3e43485bd2c111c96a756fca93389bf93
Cheesypockets <ken020789@gmail.com>
2025-07-22 20:05:52 -0400

    Adjustments to gun and slimes

--- Characters/PlayerAssets/Weapons/PlayerStarterGun.cs
+++ Characters/PlayerAssets/Weapons/PlayerStarterGun.cs
@@ -1,6 +1,16 @@
 using Godot;
 using System;
 
+/*
+ * Notes:
+ * 
+ * Need to have the bullet that will eventually be fired inherit the rotation
+ * of the gun so it fires in the proper direction.
+ * 
+ * Bullet will need to collide with mobs and make mobs take damage. 
+ */
+
+
 public partial class PlayerStarterGun : Area2D
 {
     [Export]
@@ -24,19 +34,19 @@ public partial class PlayerStarterGun : Area2D
     {
         //should both point and rotate the gun mouse. 
         //will have to figure out how to get gun not to follow mouse inside the gun offset from player
-        Vector2 mousePos = GlobalPosition.DirectionTo(GetGlobalMousePosition());
+        Vector2 mouseDirection = GlobalPosition.DirectionTo(GetGlobalMousePosition());
 
-        RotateAroundPlayer(GunPositionOffset, mousePos);
-        PointGun(mousePos);
+        RotateAroundPlayer(GunPositionOffset, mouseDirection);
+        PointGun(mouseDirection);
     }
 
-    public void RotateAroundPlayer(float offset, Vector2 mousePosition)
+    public void RotateAroundPlayer(float offset, Vector2 mouseDir)
     {
-        Position = GlobalPosition.DirectionTo(GetGlobalMousePosition()) * offset;
+        Position = mouseDir * offset;
 
     }
 
-    public void PointGun(Vector2 mousePosition)
+    public void PointGun(Vector2 mouseDir)
     {
         LookAt(GetGlobalMousePosition());
     }
